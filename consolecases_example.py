f8634244fe5f62a97c150a2458cffc31852c7f11
Naphat Amundsen <naphat@live.no>
2020-02-13 19:55:38 +0100

    Resolved uneccessary dependency (tqdm)

--- consolecases_example.py
+++ consolecases_example.py
@@ -3,7 +3,6 @@ Example module for nested cases
 '''
 from random import randint
 from time import sleep
-from tqdm import tqdm
 
 def case1(a: list):
     '''
@@ -16,6 +15,5 @@ def case2():
     '''
     Get money
     '''
-    print('Getting fat stacks:')
-    for i in tqdm(range(100)):
-        sleep(0.01)
+    print('$$$$$$')
+    sleep(0.5)

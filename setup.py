3277cc6508a0caae0c5c57e4bfa549bd72ad9688
Naphat Amundsen <naphat@live.no>
2020-10-23 23:24:41 +0200

    Updated package metadata

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Simple and sexy console interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Laohachot/PyPat-Console",
+    url="https://github.com/Napam/PyPat-Console",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",

3d6250f823fb268b9e94c6a20d16ee85b86c12a2
Naphat Amundsen <naphat@live.no>
2021-03-27 02:15:25 +0100

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pypatconsole",
-    version="0.0.15",
+    version="0.0.16",
     author="Naphat Amundsen",
     author_email="naphat@live.com",
     description="Simple and sexy console interface",

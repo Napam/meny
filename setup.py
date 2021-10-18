17edfa1a955d9ab6fd28bcd8e9a3b233c9122f2f
Naphat Amundsen <naphat@live.no>
2021-10-18 22:24:26 +0200

    bump required python version to 3.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="meny",
-    version="0.0.1",
+    version="0.0.2",
     author="Naphat Amundsen",
     author_email="naphat@live.com",
     description="Simple and sexy console interface",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
 )

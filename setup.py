1db0f9a46064f38eea5fc4b29bc7b9a300ab6146
Naphat Amundsen <naphat@live.no>
2021-12-07 20:56:56 +0100

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="meny",
-    version="0.0.5",
+    version="0.0.6",
     author="Naphat Amundsen",
     author_email="naphat@live.com",
     description="Simple and sexy console interface",
